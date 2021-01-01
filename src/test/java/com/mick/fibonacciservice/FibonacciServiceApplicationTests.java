b73b74c08c7ca9ecea533b921d3c8f4505816b2e
mick42 <30571842+mick42@users.noreply.github.com>
2021-01-01 16:34:45 +0000

    Add files via upload

---
+++ src/test/java/com/mick/fibonacciservice/FibonacciServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mick.fibonacciservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibonacciServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
